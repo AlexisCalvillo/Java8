f4a1f32964c376c8085a60544b628b016106e069
Alexis Calvillo <acalvillo@asgbpm.com>
2022-04-28 18:55:04 -0500

    Functional interface example: BiPredicate and consumer example

--- src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
+++ src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.learnJava.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,10 +13,19 @@ public class PredicateAndConsumerExample {
 
     Predicate<Student> gradeFilter = student -> student.getGradeLevel() >= 3;
     Predicate<Student> gpaFilter = student -> student.getGpa() >= 3.9;
+
+    BiPredicate<Integer, Double> biPredicate = (gradeLevel, gpaLevel)-> gradeLevel>=3 && gpaLevel>=3.9;
+
     BiConsumer<String, List<String>> printNAA = (name, activities) -> System.out.println(name + " activities: " + activities);
+
     Consumer<Student> studentConsumer = (student -> {
-        if(gradeFilter.and(gpaFilter).test(student))
+        /*if(gradeFilter.and(gpaFilter).test(student))
             printNAA.accept(student.getName(),student.getActivities());
+
+         */
+        if(biPredicate.test(student.getGradeLevel(), student.getGpa()))
+            printNAA.accept(student.getName(),student.getActivities());
+
     });
 
     public void printNameAndActiviies(List<Student> students){
