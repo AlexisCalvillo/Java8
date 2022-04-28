48538b708bf5d93a603a801d991d7fbfdc7e08a8
Alexis Calvillo <acalvillo@asgbpm.com>
2022-04-28 18:50:01 -0500

    Functional interface example: Predicate and consumer example

---
+++ src/com/learnJava/functionalInterfaces/PredicateAndConsumerExample.java
@@ -0,0 +1,32 @@
+package com.learnJava.functionalInterfaces;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PredicateAndConsumerExample {
+
+    Predicate<Student> gradeFilter = student -> student.getGradeLevel() >= 3;
+    Predicate<Student> gpaFilter = student -> student.getGpa() >= 3.9;
+    BiConsumer<String, List<String>> printNAA = (name, activities) -> System.out.println(name + " activities: " + activities);
+    Consumer<Student> studentConsumer = (student -> {
+        if(gradeFilter.and(gpaFilter).test(student))
+            printNAA.accept(student.getName(),student.getActivities());
+    });
+
+    public void printNameAndActiviies(List<Student> students){
+        students.forEach(studentConsumer);
+    }
+
+    public static void main(String[] args) {
+
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        new PredicateAndConsumerExample().printNameAndActiviies(studentList);
+    }
+
+}
