9ddf9dc819eb438600dad6e02707b5ac75775470
Alexis Calvillo <acalvillo@asgbpm.com>
2022-04-29 06:36:56 -0500

    Method reference: Refactor method reference example

---
+++ src/com/learnJava/methodreference/RefactorMethodReferenceExample.java
@@ -0,0 +1,24 @@
+package com.learnJava.methodreference;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+import java.util.function.Predicate;
+
+public class RefactorMethodReferenceExample {
+
+    static Predicate<Student> geq3 = student -> student.getGradeLevel()>=3;
+    static Predicate<Student> geq3MR = RefactorMethodReferenceExample::greaterThanGradeLevel;
+
+    public static boolean greaterThanGradeLevel(Student s){
+        return s.getGradeLevel()>=3;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(geq3.test(StudentDataBase.studentSupplier.get()));
+        
+        System.out.println(geq3MR.test(StudentDataBase.studentSupplier.get()));
+
+    }
+}
