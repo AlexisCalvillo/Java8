15a722d37b42de51086f6c00385832c650687888
Alexis Calvillo <acalvillo@asgbpm.com>
2022-05-02 06:14:27 -0500

    CollectionVsStream example

---
+++ src/com/learnJava/streams/CollectionVsStream.java
@@ -0,0 +1,31 @@
+package com.learnJava.streams;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class CollectionVsStream {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Alexis");
+        names.add("Fernanda");
+        names.add("Camila");
+
+        for(String name: names){
+            System.out.println(name);
+        }
+
+        for(String name: names){
+            System.out.println(name);
+        }
+        //names.remove(0);
+
+        System.out.println(names);
+
+        Stream<String> nameStream = names.stream();
+        nameStream.forEach(System.out::println);
+        //nameStream.forEach(System.out::println);
+
+    }
+}
