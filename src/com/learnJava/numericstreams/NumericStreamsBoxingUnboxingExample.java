38a7d7084554f9956d9d70e5df4a9f33d41243b8
Alexis Calvillo <acalvillo@asgbpm.com>
2022-05-12 21:54:54 -0500

    Numeric boxing unboxing example

---
+++ src/com/learnJava/numericstreams/NumericStreamsBoxingUnboxingExample.java
@@ -0,0 +1,26 @@
+package com.learnJava.numericstreams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamsBoxingUnboxingExample {
+    public static List<Integer> boxing(){
+        return IntStream.rangeClosed(1,10)
+                .boxed()
+                .collect(Collectors.toList());
+
+    }
+
+    public static int unboxing(List<Integer> integerList){
+        return integerList.stream()
+                //Wrapper Integer Values
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+    public static void main(String[] args) {
+        System.out.println("Boxing: " + boxing());
+
+        System.out.println("Unboxing: " + unboxing(boxing()));
+    }
+}
